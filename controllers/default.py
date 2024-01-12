daab452af7d6000e1866e273c435d573f9613c38
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-12 19:49:37 -0300

    feat(index): create route /<name>

--- controllers/default.py
+++ controllers/default.py
@@ -12,9 +12,18 @@ def index():
         senha = request.form['senha']
         cursor.execute(f"SELECT * FROM users WHERE email = '{email}' and senha = '{senha}'")
         rows = cursor.rowcount
-        if rows < 1:
+        results = cursor.fetchall()
+        print(rows)
+        print(results)
+        if rows > -1:
             alertMsg = "Email ou senha invalidos."
             alertClass = "alert alert-danger"
             return render_template('login.html', alertMsg=alertMsg, alertClass=alertClass)
-        return render_template('login.html')
+        else:
+            print(results)
+            return redirect(f'/{results[0][1]}')
     return render_template('login.html')
+
+@app.route('/<nome>', methods=['GET', 'POST'])
+def console(nome):
+    return render_template('index.html')
