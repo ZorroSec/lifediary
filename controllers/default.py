a0d8145a6a371b8a6fdb04e2cd0acfeb88099e07
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-12 19:37:51 -0300

    feat(alerts): create alert area

--- controllers/default.py
+++ controllers/default.py
@@ -2,8 +2,18 @@ from __init__ import app
 from flask import render_template
 from flask import redirect
 from flask import url_for
+from flask import request
 import mysql.connector
 from models.database import connection, cursor
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == "POST":
+        email = request.form['email']
+        senha = request.form['senha']
+        cursor.execute(f"SELECT * FROM users WHERE email = '{email}' and senha = '{senha}'")
+        rows = cursor.rowcount
+        if rows < 1:
+            alertMsg = "Email ou senha invalidos."
+            alertClass = "alert alert-danger"
+        return render_template('login.html')
     return render_template('login.html')
