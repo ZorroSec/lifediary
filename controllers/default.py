2204599fa0092c4673a0a9dcc53f09527dca5de1
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-12 21:58:50 -0300

    fix error in importing app in all python files

--- controllers/default.py
+++ controllers/default.py
@@ -1,4 +1,4 @@
-from __init__ import app
+from app import app
 from flask import render_template
 from flask import redirect
 from flask import url_for
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
-from __init__ import app
+from app import app
 
-app.run(
-    debug=True
-)
+if __name__ == "__main__":
+    app.run(
+        debug=True
+    )
